3bc4eb6b45f6226f42405fbf365cf8f2888ee070
Jatin Parmar <jatin.infomail@gmail.com>
2023-01-21 16:11:16 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class Simple {
-  public static void main(String[] args) {
+class Simple{  
+    public static void main(String args[]){ 
     System.out.println("Hello, World! this is a new commit");
     System.out.println("Hi This is a new output line 2");
   }
