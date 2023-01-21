deba34f4988c866e88a300be7231b12216ae1e84
Jatin Parmar <jatin.infomail@gmail.com>
2023-01-21 15:32:20 +0000

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class Simple {
+  public static void main(String[] args) {
+    System.out.println("Hello, World! this is a new commit");
+    System.out.println("Hi This is a new output line");
+  }
+}
